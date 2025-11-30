70ee40bd09d45488adea7e395e38d8e62f62c1bf
safi-io <m.safi.ullah@outlook.com>
2025-11-30 22:55:07 +0500

    Alembic Migrations.

---
+++ app/services/create_access_token.py
@@ -0,0 +1,10 @@
+from datetime import timedelta, datetime, UTC
+from jose import jwt, JWTError
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt

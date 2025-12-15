f1d237b3dba759cc95c0aababe0e5a0b1ad3fdff
safi-io <m.safi.ullah@outlook.com>
2025-12-15 17:23:37 +0500

    Increased Expiration Time of Access Tokens + Two EndPoints for Fetching Users.

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends, UploadFile, File
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
+from sqlalchemy.orm import Session, joinedload
 
 from app.db.session import get_db
 from app.models.user import User as user_model
+from app.models.user_profile import UserProfile as user_profile
+from app.schemas.user_profile import UserProfileWithUser
 from app.services.cloudinary import upload_image
 from app.services.get_current_user import get_current_user
 from app.services.validate_image import validate_image
@@ -30,6 +32,32 @@ def upload_picture(
     }
 
 
+@router.get("/get/me", response_model=UserProfileWithUser)
+def get_profile_with_user(current_user=Depends(get_current_user), db: Session = Depends(get_db)):
+    profile = (
+        db.query(user_profile)
+        .options(joinedload(user_profile.user))
+        .filter(user_profile.user_id == current_user.id)
+        .first()
+    )
+    if not profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+    return profile
+
+
+@router.get("/get/{user_id}", response_model=UserProfileWithUser)
+def get_profile_with_user(user_id: int, db: Session = Depends(get_db)):
+    profile = (
+        db.query(user_profile)
+        .options(joinedload(user_profile.user))
+        .filter(user_profile.user_id == user_id)
+        .first()
+    )
+    if not profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+    return profile
+
+
 @router.get("/get-all-users", summary="Returning All Users")
-def user_all(db: Session = Depends(get_db), current_user=Depends(get_current_user),):
+def user_all(db: Session = Depends(get_db), current_user=Depends(get_current_user), ):
     return db.query(user_model).all()
--- app/services/create_access_token.py
+++ app/services/create_access_token.py
@@ -7,7 +7,7 @@ from app.core.config import settings
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
-    expire = datetime.now(UTC) + (expires_delta or timedelta(minutes=15))
+    expire = datetime.now(UTC) + (expires_delta or timedelta(minutes=10080))
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
